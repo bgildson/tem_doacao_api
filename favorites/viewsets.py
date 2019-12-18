e0b47ad320b23c933f1e9f39fd25a196308907f8
bgildson <gildson13@hotmail.com>
2019-12-18 16:17:15 -0300

    added filter and ordenation

--- favorites/viewsets.py
+++ favorites/viewsets.py
@@ -8,9 +8,10 @@ from .serializers import FavoriteSerializer
 class FavoritesViewSet(ReadOnlyModelViewSet):
     serializer_class = FavoriteSerializer
     permission_classes = (IsAuthenticated,)
+    filterset_fields = ('donation__id',)
+    ordering = ('-at',)
 
     def get_queryset(self):
         return Favorite.objects \
             .filter(user=self.request.user) \
-            .order_by('-at') \
             .all()
