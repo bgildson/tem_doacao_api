fe96bab93422a7d5a277ff4a546b738ee5b357cc
bgildson <gildson13@hotmail.com>
2020-01-03 15:43:49 -0300

    created tests for categories resource

--- categories/tests.py
+++ categories/tests.py
@@ -1,3 +1,25 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase, APIClient
 
-# Create your tests here.
+from .models import Category
+
+
+class CategoriesTest(APITestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.category = Category.objects.create(name='category name')
+
+    def test_anon_user_can_get_categories(self):
+        url = reverse('categories-list')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_anon_user_can_get_categories_by_id(self):
+        category_id = str(CategoriesTest.category.id)
+        url = reverse('categories-detail', args=[category_id])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
